bfcf394742e285ce1ec95a4a80a4d18ebbf4e55f
Riccardo Poli <S4522674@studenti.unige.it>
2022-01-01 23:32:21 +0100

    Piccolo Miglioramento

--- Frequency_list_attempt.py
+++ Frequency_list_attempt.py
@@ -7,7 +7,7 @@ from Graph import Graph
 from Node import Node
 
 start_time = time.time()
-df = pd.read_csv("datasets/db_10000.csv", dtype=str)
+df = pd.read_csv("datasets/db_100.csv", dtype=str)
 df = df.drop(["id","disease"],1)
 
 print("---------------------------------------------------")
@@ -94,6 +94,7 @@ root_node_qi = ["age", "zip_code", "city_birth"]
 #[freq_list, counter] = get_frequency_list_pandas(df, root_node_qi)
 
 print(freq_list)
+print("\n")
 print("unique tuples: " + str(counter))
 print("-----------------------------------------------------")
 
@@ -130,3 +131,5 @@ print("unique tuples: " + str(counter))
 print("-----------------------------------------------------")
 
 '''
+
+print("Execution time: "+str(time.time() - start_time)+"s")
